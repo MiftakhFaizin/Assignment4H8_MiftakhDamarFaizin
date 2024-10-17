fc8d023a822b63d80e48ff50dce9019a3912363f
MiftakhFaizin <miftakhdamar07@gmail.com>
2024-10-18 00:01:21 +0700

    push

---
+++ script_form.js
@@ -0,0 +1,32 @@
+let formNama = document.querySelector("#form-nama")
+let formRole = document.querySelector("#form-role")
+let formAvailability = document.querySelector("#form-availability")
+let formUsia = document.querySelector("#form-usia")
+let formLokasi = document.querySelector("#form-lokasi")
+let formPengalaman = document.querySelector("#form-pengalaman")
+let formEmail = document.querySelector("#form-email")
+let btnSubmit = document.querySelector("#btn-submit")
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    // mengisi nilai default form berdasarkan data yang ada di local storage
+    formNama.value = localStorage.getItem('nama');
+    formRole.value = localStorage.getItem('role');
+    formAvailability.value = localStorage.getItem('availability');
+    formUsia.value = localStorage.getItem('usia');
+    formLokasi.value = localStorage.getItem('lokasi');
+    formPengalaman.value = localStorage.getItem('pengalaman');
+    formEmail.value = localStorage.getItem('email');
+});
+
+btnSubmit.addEventListener("click", () => {
+    localStorage.setItem('nama', formNama.value)
+    localStorage.setItem('role', formRole.value)
+    localStorage.setItem('availability', formAvailability.value)
+    localStorage.setItem('usia', formUsia.value);
+    localStorage.setItem('lokasi', formLokasi.value)
+    localStorage.setItem('pengalaman', formPengalaman.value)
+    localStorage.setItem('email', formEmail.value)
+    
+    window.location.href = "section_profil.html"
+})
---
+++ script_profil.js
@@ -0,0 +1,36 @@
+let body = document.querySelector("body")
+let btnEdit = document.querySelector("#btn-edit")
+
+let nama = document.querySelector("#nama")
+let role = document.querySelector("#role")
+let availability = document.querySelector("#availability")
+let usia = document.querySelector("#usia")
+let lokasi = document.querySelector("#lokasi")
+let pengalaman = document.querySelector("#pengalaman")
+let email = document.querySelector("#email")
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    // mengisi data profil sekarang berdasarkan data yang ada di local storage
+    nama.innerText = localStorage.getItem('nama');
+    role.innerText = localStorage.getItem('role');
+    availability.innerText = localStorage.getItem('availability');
+    usia.innerText = localStorage.getItem('usia');
+    lokasi.innerText = localStorage.getItem('lokasi');
+    pengalaman.innerText = localStorage.getItem('pengalaman');
+    email.innerText = localStorage.getItem('email');
+});
+
+btnEdit.addEventListener("click", () => {
+
+    //mengirim data sekarang ke local storage
+    localStorage.setItem('nama', nama.innerText)
+    localStorage.setItem('role', role.innerText)
+    localStorage.setItem('availability', availability.innerText)
+    localStorage.setItem('usia', usia.innerText);
+    localStorage.setItem('lokasi', lokasi.innerText)
+    localStorage.setItem('pengalaman', pengalaman.innerText)
+    localStorage.setItem('email', email.innerText)
+    
+    window.location.href = "section_form.html"
+})
